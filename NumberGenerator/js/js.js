5a495bfe6d3af63fc05e86364297580aca33eafb
serenawang0913 <serena0913@gmail.com>
2016-05-04 17:26:00 +0800

    1. Modify icon. 2. Remove reload button.

--- NumberGenerator/js/js.js
+++ NumberGenerator/js/js.js
@@ -2,7 +2,7 @@ var numMax = 40;
 var numAry = [];
 function initNumAry() {
 	for (var i=0;i < numMax;i++) {
-		numAry[i] = i;
+		numAry[i] = (i/1+1);
 	}
 }
 function go() {
@@ -24,7 +24,7 @@ function reloadHomeAppendTS() {
 	var gHomeTS = (String(location.search).split("=")[1])?String(location.search).split("=")[1]:0;
 	//console.log(gHomeTS);
 	var gCurrTS = $.now();
-	var homepage = "home.html";
+	var homepage = "index.html";
 	if ((gCurrTS - gHomeTS) > 500) {
 		top.location = homepage+"?_ts="+gCurrTS;
 	}
